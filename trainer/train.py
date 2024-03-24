3cba9ed0c611c5576a57d4d715457374d366a73d
zlcython-by-zlc1003 <107649766+zlcython-by-zlc1003@users.noreply.github.com>
2024-03-24 10:47:58 -0700

    new

--- trainer/train.py
+++ trainer/train.py
@@ -17,7 +17,7 @@ from utils import CTCLabelConverter, AttnLabelConverter, Averager
 from dataset import hierarchical_dataset, AlignCollate, Batch_Balanced_Dataset
 from model import Model
 from test__ import validation
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+device = torch.device("mps" if torch.backends.mps.is_available() else ('cuda' if torch.cuda.is_available() else 'cpu'))
 
 def count_parameters(model):
     print("Modules, Parameters")
