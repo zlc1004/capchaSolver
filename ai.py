740bde7d9742751d558f70a6029340d5c5351aed
zlcython-by-zlc1003 <107649766+zlcython-by-zlc1003@users.noreply.github.com>
2024-03-24 10:14:52 -0700

    faster ai

--- ai.py
+++ ai.py
@@ -5,17 +5,14 @@ import random,tqdm
 def randomString():
     rndLetters = (random.choice("qwertyuiopasdfghjklzxcvbnm1234567890") for _ in range(5))
     return "".join(rndLetters)
-
+reader = easyocr.Reader(['en'],recog_network="iter_10000") # this needs to run only once to load the model into memory
 out=""
-for i in tqdm.tqdm(range(5)):
+for i in tqdm.tqdm(range(10)):
     for i in (range(1)):
         string=randomString()
         c = Claptcha(string, "Consolas.ttf", noise=0.1)
         # c.write(""+string+'.png')
         c.write("./valid/"+string+'.png')
-
-
-    reader = easyocr.Reader(['en'],recog_network="iter_10000") # this needs to run only once to load the model into memory
     result = reader.readtext("./valid/"+string+'.png')
     out+=(result[0][1])
     out+="\t"
