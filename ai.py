1bd2a286f85b02ddceaf876457138a7707f04d96
zlcython-by-zlc1003 <107649766+zlcython-by-zlc1003@users.noreply.github.com>
2024-03-24 11:02:47 -0700

    better testing

--- ai.py
+++ ai.py
@@ -1,3 +1,6 @@
+CHECKS=5
+
+
 from claptcha import Claptcha
 import easyocr
 from PIL import Image
@@ -7,15 +10,20 @@ def randomString():
     return "".join(rndLetters)
 reader = easyocr.Reader(['en'],recog_network="iter_10000") # this needs to run only once to load the model into memory
 out=""
-for i in tqdm.tqdm(range(25)):
-    for i in (range(1)):
-        string=randomString()
-        c = Claptcha(string, "Consolas.ttf", noise=0.1)
-        # c.write(""+string+'.png')
-        c.write("./valid/"+string+'.png')
+correct=0
+for i in tqdm.tqdm(range(CHECKS)):
+    string=randomString()
+    c = Claptcha(string, "Consolas.ttf", noise=0.1)
+    # c.write(""+string+'.png')
+    c.write("./valid/"+string+'.png')
     result = reader.readtext("./valid/"+string+'.png')
     out+=(result[0][1])
     out+="\t"
     out+=(string)
+    out+="\t"
+    out+=str(string==result[0][1])
     out+="\n"
+    if string==result[0][1]:
+        correct+=1
 print(out)
+print("Accuracy: ",(correct/CHECKS)*100,"%")
