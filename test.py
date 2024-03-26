aff77cfdc35ba4457cd39960e135258f1d0ac5c5
zlcython-by-zlc1003 <107649766+zlcython-by-zlc1003@users.noreply.github.com>
2024-03-26 12:41:38 -0700

    test

--- test.py
+++ test.py
@@ -1,3 +1,11 @@
-import betterClaptcha
-cap=betterClaptcha.Claptcha("test", "Consolas.ttf",color=(0,255,0),noise=0.3)
-cap.image[1].show()
+from betterClaptcha import Claptcha
+import random,tqdm
+def randomColor():
+    return (random.randint(0,255),random.randint(0,255),random.randint(0,255))
+def randomString():
+    rndLetters = (random.choice("qwertyuiopasdfghjklzxcvbnm1234567890") for _ in range(random.randint(4, 8)))
+    return "".join(rndLetters)
+for i in tqdm.tqdm(range(10)):
+    string=randomString()
+    c = Claptcha(string, "Consolas.ttf",size=(int(200*1.5), int(80*1.5)), noise=random.random()/3,lines=random.randint(1,3),color=randomColor())
+    c.write("./valid/"+string+'.png')
